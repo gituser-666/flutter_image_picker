8173c0276496b50e2a54a2813a44638b871bc3c0
weihaizhou <weihaizhou@jd.com>
2019-09-17 20:16:51 +0800

    修复拍照线程异常

---
+++ example/build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: D:\weihaizhou\soft_lib\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\weihaizhou\soft_lib\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\weihaizhou\workspace\image_picker-0.4.12+1\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\weihaizhou\soft_lib\flutter\bin\cache\artifacts\engine\ios"
